d2776a4b7598156b10e1eaaa087c774e8eadcaf5
syladiana <sylasudiana@gmail.com>
2021-01-31 12:29:32 +0700

    Revisi Final Project

---
+++ Final-Project/final_project_revisi.py
@@ -0,0 +1,35 @@
+# Source : 
+#     - https://www.youtube.com/watch?v=sXjpkcF7rPQ
+#     - https://code.tutsplus.com/tutorials/sending-emails-in-python-with-smtp--cms-29975
+
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email import encoders
+
+file = open('e:\Indonesian AI\Basic-python-b3\Final-project/receiver_list.txt', 'r').read()
+flx = file.split()
+
+sender_email = "senjacakraningrat@gmail.com"
+rec_email = flx
+body = "Hello, this was sent using Python"
+
+msg = MIMEMultipart()
+msg['From'] = sender_email
+msg['To'] = ', '.join(flx)
+msg['Subject'] = 'TEST SUBJECT'
+
+msg.attach(MIMEText(body, 'plain'))
+text = msg.as_string()
+
+server = smtplib.SMTP('smtp.gmail.com', 587)
+server.starttls()
+
+password = input(str("Masukan Password :"))
+server.login(sender_email, password)
+print("Login success!")
+server.sendmail(sender_email, rec_email, text)
+server.quit()
+
+print("Email has been sent to ", rec_email)
